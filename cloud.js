9b60bf5be3f6b1f2da8ae5142a809335fc89b99a
tanglei11 <521017818@qq.com>
2017-07-30 21:50:39 +0800

    commit 2017-07-30
    POI保存和读取接口完成

--- cloud.js
+++ cloud.js
@@ -4,6 +4,7 @@ var AV = require('leanengine');
  * 一个简单的云代码方法
  */
 var Banner = AV.Object.extend('Banner');
+var Place = AV.Object.extend('Place');
 
 AV.Cloud.define('saveBanner', function(request) {
 	var banner = new Banner();
@@ -20,6 +21,33 @@ AV.Cloud.define('saveBanner', function(request) {
 AV.Cloud.define('getBanner', function(request) {
   var query = new AV.Query(Banner);
   return query.find().then(function(results) {
+    return results;
+  }).catch(function(error) {
+    throw new AV.Cloud.Error('查询失败');
+  });
+});
+
+AV.Cloud.define('savePlace',function(request){
+	var place = new Place();
+	place.set('name',request.params.name);
+	place.set('longitude',request.params.longitude);
+	place.set('latitude',request.params.latitude);
+	place.set('address',request.params.address);
+	place.set('phone',request.params.phone);
+	place.set('imgs',request.params.imgs);
+	place.set('bgImg',request.params.bgImg);
+	place.set('introduce',request.params.introduce);
+	place.set('placeType',request.params.placeType);
+	place.save().then(function(pla){
+		console.log('objectId is' + pla.id);
+	},function(error){
+		console.error(error);
+	});
+});
+
+AV.Cloud.define('getPlaces',function(request){
+	var query = new AV.Query(Place);
+	return query.find().then(function(results) {
     return results;
   }).catch(function(error) {
     throw new AV.Cloud.Error('查询失败');
